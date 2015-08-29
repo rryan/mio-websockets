da8b7c78b3b5a2a199368597a2e9f6f78a479fd9
Aaron Burrow <burrows@Aarons-MacBook-Pro.local>
2015-08-29 11:07:07 -0400

    able to accept a client and then disconnect him

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,74 @@ enum InternalMessage {
     Data{client_token: Token, format: String, data: String},
 }
 
+struct DataFrame {
+    op_code: OpCode,
+    payload_len: u64,
+    masking_key: [u8; 4],
+    payload: Vec<u8>
+}
+
+/// Decodes OpCode
+pub const OP_CODE_UN_MASK: u8 = 0b0000_1111;
+
+/// Encodes OpCode
+pub const OP_CODE_MASK: u8 = 0b1000_0000;
+
+/// Decodes payload
+pub const PAYLOAD_KEY_UN_MASK: u8 = 0b0111_1111;
+
+/// Continuation Op byte
+pub const OP_CONTINUATION: u8 = 0x0;
+
+/// Text Op byte
+pub const OP_TEXT: u8 = 0x1;
+
+/// Binary Op byte
+pub const OP_BINARY: u8 = 0x2;
+
+/// Close Op byte
+pub const OP_CLOSE: u8 = 0x8;
+
+/// Ping Op byte
+pub const OP_PING: u8 = 0x9;
+
+/// Pong Op byte
+pub const OP_PONG: u8 = 0xA;
+
+pub enum OpCode {
+    /// Continuation frame from last packed
+    Continuation,
+
+    /// UTF-8 text data
+    Text,
+
+    /// Binary data as u8
+    Binary,
+
+    /// Indicates client has closed connection
+    Close,
+
+    /// Heartbeat requested from client
+    Ping,
+
+    /// Heartbeat response to ping frame
+    /// Can also be sent without a ping request
+    Pong,
+}
+
+impl fmt::Display for OpCode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            OpCode::Continuation => "Continuation".fmt(f),
+            OpCode::Text => "Text".fmt(f),
+            OpCode::Binary => "Binary".fmt(f),
+            OpCode::Close => "Close".fmt(f),
+            OpCode::Ping => "Ping".fmt(f),
+            OpCode::Pong => "Pong".fmt(f),
+        }
+    }
+}
+
 fn gen_key(key: &String) -> String {
     let mut m = sha1::Sha1::new();
     let mut buf = [0u8; 20];
@@ -61,6 +129,7 @@ impl ParserHandler for HttpParser {
 }
 
 #[derive(PartialEq)]
+#[derive(Debug)]
 enum ClientState {
     AwaitingHandshake,
     HandshakeResponse,
@@ -80,7 +149,7 @@ struct WebSocketClient {
     state: ClientState,
     writing_substate: SubState,
     reading_substate: SubState,
-    outgoing_messages: std::collections::LinkedList<String>,
+    outgoing_messages: std::collections::LinkedList<InternalMessage>,
 }
 
 impl WebSocketClient {
@@ -102,16 +171,84 @@ impl WebSocketClient {
         }
     }
 
+    // /////////////////////
+    //   Writing to Client
+    // /////////////////////
     fn handshake_response(&mut self) {
-        let headers = self.headers.borrow();
+        let headers      = self.headers.borrow();
         let response_key = gen_key(&headers.get("Sec-WebSocket-Key").unwrap());
-        let response = fmt::format(format_args!("HTTP/1.1 101 Switching Protocols\r\n\
-                                                 Connection: Upgrade\r\n\
-                                                 Sec-WebSocket-Accept: {}\r\n\
-                                                 Upgrade: websocket\r\n\r\n", response_key));
+        let response     = fmt::format(
+            format_args!("HTTP/1.1 101 Switching Protocols\r\n\
+                         Connection: Upgrade\r\n\
+                         Sec-WebSocket-Accept: {}\r\n\
+                         Upgrade: websocket\r\n\r\n", response_key));
         self.socket.try_write(response.as_bytes()).unwrap();
     }
 
+    fn write_response(&mut self, op: OpCode, payload: &mut Vec<u8>) {
+        let mut out_buf: Vec<u8> = Vec::with_capacity(payload.len() + 9);
+
+        self.set_op_code(&op, &mut out_buf);
+        self.set_payload_info(payload.len(), &mut out_buf);
+
+        // TODO - Fix with Vec.append() once stable
+        // out_buf.append(payload);
+        for byte in payload.iter() {
+            out_buf.push(*byte);
+        }
+
+        self.socket.try_write(&out_buf).unwrap();
+    }
+
+    fn set_op_code(&self, op: &OpCode, buf: &mut Vec<u8>) {
+        let op_code = match *op {
+            OpCode::Continuation    => OP_CONTINUATION,
+            OpCode::Text            => OP_TEXT,
+            OpCode::Binary          => OP_BINARY,
+            OpCode::Close           => OP_CLOSE,
+            OpCode::Ping            => OP_PING,
+            OpCode::Pong            => OP_PONG
+        };
+        buf.push(op_code | OP_CODE_MASK);
+    }
+
+    fn set_payload_info(&self, len: usize, buf: &mut Vec<u8>) {
+        if len <= 125 {
+            buf.push(len as u8);
+        } else if len <= 65535 {
+            let mut len_buf = [0u8; 2];
+            len_buf[0] = (len as u16 & 0b1111_1111u16 << 8) as u8;
+            len_buf[1] = (len as u16 & 0b1111_1111 as u16) as u8;
+
+            buf.push(126u8); // 16 bit prelude
+            buf.push(len_buf[0]);
+            buf.push(len_buf[1]);
+        } else {
+            let mut len_buf = [0u8; 8];
+            len_buf[0] = (len as u64 & 0b1111_1111u64 << 56) as u8;
+            len_buf[1] = (len as u64 & 0b1111_1111u64 << 48) as u8;
+            len_buf[2] = (len as u64 & 0b1111_1111u64 << 40) as u8;
+            len_buf[3] = (len as u64 & 0b1111_1111u64 << 32) as u8;
+            len_buf[4] = (len as u64 & 0b1111_1111u64 << 24) as u8;
+            len_buf[5] = (len as u64 & 0b1111_1111u64 << 16) as u8;
+            len_buf[6] = (len as u64 & 0b1111_1111u64 << 8) as u8;
+            len_buf[7] = (len as u64 & 0b1111_1111u64) as u8;
+
+            buf.push(127u8); // 64 bit prelude
+            buf.push(len_buf[0]);
+            buf.push(len_buf[1]);
+            buf.push(len_buf[2]);
+            buf.push(len_buf[3]);
+            buf.push(len_buf[4]);
+            buf.push(len_buf[5]);
+            buf.push(len_buf[6]);
+            buf.push(len_buf[7]);
+        }
+    }
+
+    // /////////////////////
+    //  Reading from Client
+    // /////////////////////
     fn handshake_request(&mut self) -> bool {
         loop {
             let mut buf = [0; 2048];
@@ -196,9 +333,10 @@ impl InternalWriter {
     }
 
     fn write(&mut self, msg: InternalMessage) {
-        let v = vec![1];
-        self.pipe_writer.write(&v).unwrap();
-
+        // FIXME: correct way to handle `poke`?
+        let poke = "a";
+        self.pipe_writer.write(poke.to_string().as_bytes()).unwrap();
+        self.pipe_writer.flush();
         self.input_tx.send(msg).unwrap();
     }
 }
@@ -264,8 +402,8 @@ impl Handler for WebSocketServer {
     fn ready(&mut self, event_loop: &mut EventLoop<WebSocketServer>,
              token: Token, events: EventSet) {
         let mut reregister_token : Option<Token> = None;
+        println!("TOKEN: {:?}", token);
         if events.is_readable() {
-            println!("SERVER_TOKEN: {:?}", self.server_token);
             if token == self.server_token {
                 let client_socket = match self.socket.accept() {
                     Ok(Some(sock)) => sock,
@@ -282,25 +420,26 @@ impl Handler for WebSocketServer {
                 event_loop.register_opt(&self.clients[&new_token].socket,
                                         new_token, EventSet::readable(),
                                         PollOpt::edge() | PollOpt::oneshot()).unwrap();
-                self.output_tx.send(InternalMessage::NewClient{client_token: new_token}).unwrap();
             } else if token == self.pipe_token {
-                let mut buff = Vec::new();
-                let size = self.pipe_reader.read(&mut buff);
-                println!("READ: {:?} {:?}", size, buff);
-                println!("PIPE_TOKEN: {:?}", self.pipe_token);
-                match self.input_rx.recv().unwrap() {
-                    InternalMessage::CloseClient{client_token: _token} => {
-                        // FIXME: implement
-                    },
-                    InternalMessage::Data{client_token: token, data, format: _} => {
-                        let client = self.clients.get_mut(&token).unwrap();
-                        client.outgoing_messages.push_back(data);
-                        if client.state == ClientState::Open {
-                            client.writing_substate = SubState::Doing;
-                            reregister_token = Some(token);
+                // FIXME: handle a broken pipe (ie, reads zero bytes)
+                self.pipe_reader.by_ref().take(1).read_to_end(&mut Vec::new()).unwrap();
+                let msg = self.input_rx.recv();
+                match msg {
+                    Ok(InternalMessage::NewClient{client_token: _}) => {},
+                    Err(_e) => {},
+                    Ok(InternalMessage::CloseClient{client_token: token}) |
+                        Ok(InternalMessage::Data{client_token: token, format: _, data: _}) => {
+                        match self.clients.get_mut(&token) {
+                            Some(client) => {
+                                if client.state == ClientState::Open {
+                                    client.outgoing_messages.push_back(msg.unwrap());
+                                    client.writing_substate = SubState::Doing;
+                                    reregister_token = Some(token);
+                                }
+                            },
+                            None => println!("tried sending message to non-existent client!"),
                         }
                     },
-                    _ => {},
                 }
             } else {
                 let mut client = self.clients.get_mut(&token).unwrap();
@@ -328,8 +467,23 @@ impl Handler for WebSocketServer {
                         client.handshake_response();
                         client.state      = ClientState::Open;
                         reregister_token  = Some(token);
+
+                        self.output_tx.send(InternalMessage::NewClient{client_token: token}).unwrap();
                     },
-                    ClientState::Open => {},
+                    ClientState::Open => {
+                        assert!(client.outgoing_messages.len() > 0);
+                        match client.outgoing_messages.pop_front() {
+                            Some(InternalMessage::Data{client_token: token, format, data}) => {
+                                // client.write_response(OpCode::Text, &mut m.into_bytes());
+                            },
+                            Some(InternalMessage::CloseClient{client_token: token}) => {
+                                client.write_response(OpCode::Close, &mut vec!());
+                                client.writing_substate = SubState::Waiting;
+                            },
+                            Some(InternalMessage::NewClient{client_token: _token}) => unreachable!(),
+                            None => unreachable!(),
+                        }
+                    },
                 }
             }
         }
@@ -366,32 +520,33 @@ impl Handler for WebSocketServer {
 fn main() {
     let (mut server, mut reader, mut writer) = WebSocketServer::new("0.0.0.0", 10000);
 
-    // Receiver Process
     thread::spawn(move || {
-        loop {
-            match reader.bread() {
-                InternalMessage::NewClient{client_token: token} => {
-                    println!("New Client: {:?}", token);
-                },
-                InternalMessage::CloseClient{client_token: token} => {
-                    println!("Close Client: {:?}", token);
-                },
-                InternalMessage::Data{client_token: _token, format: _format, data: _data} =>
-                    {},
-            }
+        // Wait for new client
+        match reader.bread() {
+            InternalMessage::NewClient{client_token: token} => {
+                println!("New Client: {:?}", token);
+            },
+            _ => panic!("123"),
         }
-    });
 
-    // Sender Process
-    thread::spawn(move || {
-        loop {
-            loop {}
-            let msg = InternalMessage::Data{client_token: Token(2),
-                format: String::from("Aaron"),
-                data:   String::from("Burrow"),
-            };
-            writer.write(msg);
+        // disconnect
+        writer.write(InternalMessage::CloseClient{client_token: Token(2)});
+
+        loop {}
+
+        /*
+        // Receive
+        match reader.bread() {
+            InternalMessage::NewClient{client_token: token} => {
+                println!("New Client: {:?}", token);
+            },
+            InternalMessage::CloseClient{client_token: token} => {
+                println!("Close Client: {:?}", token);
+            },
+            InternalMessage::Data{client_token: _token, format: _format, data: _data} =>
+                {},
         }
+        */
     });
 
     server.start();
